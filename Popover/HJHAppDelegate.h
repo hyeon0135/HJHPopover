4e9fbdd977e08441c9726538f23a0b919d10fd1f
Jonghwan Hyeon <hyeon0135@gmail.com>
2013-03-15 21:48:37 +0900

    Initial Commit

---
+++ Popover/HJHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HJHAppDelegate.h
+//  Popover
+//
+//  Created by Jonghwan Hyeon on 3/15/13.
+//  Copyright (c) 2013 Jonghwan Hyeon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HJHAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Popover/HJHAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  HJHAppDelegate.m
+//  Popover
+//
+//  Created by Jonghwan Hyeon on 3/15/13.
+//  Copyright (c) 2013 Jonghwan Hyeon. All rights reserved.
+//
+
+#import "HJHAppDelegate.h"
+
+@implementation HJHAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
